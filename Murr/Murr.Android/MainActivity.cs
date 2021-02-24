5390996d83eff09aaed839ccee2d3dfbf84781d0
Anton <Antonfirsov@list.ru>
2021-02-24 18:46:14 +0300

    Getting started/Pre-Design

--- Murr/Murr.Android/MainActivity.cs
+++ Murr/Murr.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using MediaManager;
 
 namespace Murr.Droid
 {
@@ -21,6 +22,7 @@ namespace Murr.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            CrossMediaManager.Current.Init(this);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
---
+++ Murr/Murr/ViewModel/LandingViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Murr.ViewModel
+{
+    public class LandingViewModel : BaseViewModel
+    {
+    }
+}
