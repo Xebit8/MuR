f5336a26ab5eb25494b7519a747837ce4a76becd
Anton <Antonfirsov@list.ru>
2021-03-13 13:35:10 +0300

    Small changes

--- MuR/MuR/View/LandingPage.xaml.cs
+++ MuR/MuR/View/LandingPage.xaml.cs
@@ -3,18 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Murr.View;
+using MediaManager;
+using SpotifyAPI.Web;
+
 
 namespace Murr.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LandingPage : ContentPage
     {
+
         public LandingPage()
         {
             InitializeComponent();
         }
+        //public async void ToPlay(object sender, EventArgs e)
+        //{
+            
+        //}
+        //public async void ToPause(object sender, EventArgs e)
+        //{
+            //await CrossMediaManager.Current.Pause();
+        //}
+
     }
 }
