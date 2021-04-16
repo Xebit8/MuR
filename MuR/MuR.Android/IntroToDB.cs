683a4a75009df1b9a5ff729ddb021c28f376d20b
VeneS-Sudo <veryworks9@gmail.com>
2021-04-16 23:09:34 +0300

    introduction sqlite db

---
+++ MuR/MuR.Android/IntroToDB.cs
@@ -0,0 +1,24 @@
+﻿using MuR.Model;
+using Xamarin.Forms;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+[assembly: Dependency(typeof(Murr.Droid.IntroToDB))]
+namespace Murr.Droid
+{
+    
+    class IntroToDB : IToSQLiteDB
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var connection = new SQLiteAsyncConnection(new Murr.Droid.FileSystemImplementation().GetExternalDirectory(typeFiles.DataBase));
+            return connection;
+        }
+    }
+}
---
+++ MuR/MuR/Model/DatabaseRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using SQLite;
+using System.Threading.Tasks;
+using MuR.Model.SQLiteObjects;
+
+namespace MuR.Model
+{
+    /// <summary>
+    /// класс для работы с базой данных
+    /// </summary>
+    public class DatabaseRepository
+    {
+        /// <summary>
+        /// соединение с базой данных
+        /// </summary>
+        private  readonly SQLiteAsyncConnection dbConnection;
+        public SQLiteAsyncConnection DBConnection => dbConnection;
+
+        public DatabaseRepository()
+        {
+            dbConnection = DependencyService.Get<IToSQLiteDB>().GetConnection();
+        }
+
+        public async Task<CreateTableResult> CreateTable()
+        {
+            return await DBConnection.CreateTableAsync<Genre>();
+        }
+        public async Task<List<SQLiteConnection.ColumnInfo>> GetAuthor()
+        {
+            return await DBConnection.GetTableInfoAsync("genre");
+        }
+    }
+}
