4bdf869a1cc8be89bf097f7824acd17958451d67
Anton <Antonfirsov@list.ru>
2021-04-22 12:57:38 +0300

    small changes

--- MuR/MuR/View/LandingPage.xaml.cs
+++ MuR/MuR/View/LandingPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,9 @@ namespace Murr.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LandingPage : ContentPage
     {
+        public const string PlayCircleOutline = "\U000f040d";
+        public const string PauseCircleOutline = "\U000f03e6";
+
         public LandingPage()
         {
             InitializeComponent();
@@ -27,9 +31,76 @@ namespace Murr.View
             var files = (await FilePicker.PickMultipleAsync(CrossFileManipulation.optionsPicket)).ToArray();
             CrossFileManipulation.LoadToCache(files);
         }
+        private async void ButtonPlaySong_Clicked(object sender, EventArgs e)
+        {
+            var selectedAudioItem = (((Button)sender).BindingContext as AudioItem);
+            if (selectedAudioItem == null)
+                return;
+
+            var result = await CrossMediaManager.Current.PlayQueueItem(selectedAudioItem.Number - 1);
+
+            SetupCurrentItemDetails();
+
+            if (!CrossMediaManager.Current.IsPlaying())
+                await CrossMediaManager.Current.Play();
+        }
+        private void SetupPlaylist(bool isListRefresh)
+        {
+            List<IMediaItem> queueList = CrossMediaManager.Current.Queue.MediaItems.ToList();
+
+            ObservableCollection<AudioItem> list = new ObservableCollection<AudioItem>();
+            for (int i = 0; i < queueList.Count; i++)
+            {
+                IMediaItem item = queueList[i];
+
+                string title = "-"; string artist = "-";
+
+                if (!string.IsNullOrEmpty(item.DisplayTitle))
+                    title = $"{item.DisplayTitle.ToUpper()}";
+
+                if (!string.IsNullOrEmpty(item.Artist))
+                    artist = $"{item.Artist.ToUpper()}";
+
+                list.Add(new AudioItem()
+                {
+                    Title = title,
+                    Artist = artist,
+                    Number = i + 1
+                });
+            }
+
+            CollectionView.ItemsSource = list;
+
+            if (isListRefresh)
+                CollectionView.ScrollTo(list.Last());
+        }
+
+        private void SetupCurrentItemDetails()
+        {
+            IMediaItem currentAudioItem = CrossMediaManager.Current.Queue.Current;
+
+            // Media item details
+            var displayDetails = string.Empty;
+            if (!string.IsNullOrEmpty(currentAudioItem.DisplayTitle))
+                displayDetails = currentAudioItem.DisplayTitle;
+
+            if (!string.IsNullOrEmpty(currentAudioItem.Artist))
+                displayDetails = $"{displayDetails} - {currentAudioItem.Artist}";
+
+            LabelCurrentTrackTitle.Text = displayDetails.ToUpper();
+
+            LabelCurrentTrackIndex.Text = $"CURRENT TRACK: {CrossMediaManager.Current.Queue.CurrentIndex + 1}/{CrossMediaManager.Current.Queue.Count}";
+        }
+
+        public class AudioItem
+        {
+            public string Title { get; set; }
+            public string Artist { get; set; }
+            public int Number { get; set; }
+        }
         public async void ToPlayerPage(object sender, EventArgs args)
         {
-            await Navigation.PushAsync(new PlayerPage());
+                await Navigation.PushAsync(new PlayerPage());
         }
 
     }
--- MuR/MuR/View/PlayerPage.xaml.cs
+++ MuR/MuR/View/PlayerPage.xaml.cs
@@ -11,6 +11,11 @@ using MediaManager;
 using Xamarin.Essentials;
 using MuR.Model;
 using MediaManager.Library;
+using MediaManager.Media;
+using MediaManager.Playback;
+using MediaManager.Player;
+using MediaManager.Queue;
+using PositionChangedEventArgs = MediaManager.Playback.PositionChangedEventArgs;
 
 namespace Murr.View
 {
@@ -18,11 +23,14 @@ namespace Murr.View
     public partial class PlayerPage : ContentPage
     {
         int play_counter = 0;
-        int menu_counter = 0;
 
         public PlayerPage()
         {
             InitializeComponent();
+
+            CrossMediaManager.Current.StateChanged += Current_OnStateChanged;
+            CrossMediaManager.Current.PositionChanged += Current_PositionChanged;
+            CrossMediaManager.Current.MediaItemChanged += Current_MediaItemChanged;
         }
         public async void PickAudioFile(object sender, EventArgs args)
         {
@@ -33,9 +41,6 @@ namespace Murr.View
         {
             foreach (var item in CrossFileManipulation.LoadFromExternalCache())
                 CrossMediaManager.Current.Queue.Add(item);
-
-            IMediaItem currentAudioItem = CrossMediaManager.Current.Queue.Current;
-            SongData(currentAudioItem);
 
             play_counter++;
 
@@ -58,39 +63,71 @@ namespace Murr.View
                 await CrossMediaManager.Current.Play();
             }
         }
-        public async void SkipFwd(object sender, EventArgs args)
+        private async void SkipFwd(object sender, EventArgs args)
         {
             await CrossMediaManager.Current.PlayNext();
         }
-        public async void SkipBack(object sender, EventArgs args)
+        private async void SkipBack(object sender, EventArgs args)
         {
             await CrossMediaManager.Current.PlayPrevious();
         }
-        public void SongData(IMediaItem currentAudioItem)
+        private void SetupCurrentMediaDetails(IMediaItem currentMediaItem)
         {
+            // Set up Media item details in UI
+            var songDetails = string.Empty;
+            if (!string.IsNullOrEmpty(currentMediaItem.DisplayTitle))
+                songDetails = currentMediaItem.DisplayTitle;
 
-            song_label.Text = currentAudioItem.Title;
-            artist_label.Text = currentAudioItem.Artist;
+            if (!string.IsNullOrEmpty(currentMediaItem.Artist))
+                songDetails = $"{songDetails} - {currentMediaItem.Artist}";
+
+            song_label.Text = songDetails.ToUpper();
+        }
+        private void SetupCurrentMediaPlayerState(MediaPlayerState currentPlayerState)
+        {
+            if (currentPlayerState == MediaManager.Player.MediaPlayerState.Loading)
+            {
+                SongSlider.Value = 0;
+            }
+            else if (currentPlayerState == MediaManager.Player.MediaPlayerState.Playing
+                    && CrossMediaManager.Current.Duration.Ticks > 0)
+            {
+                SongSlider.Maximum = CrossMediaManager.Current.Duration.Ticks;
+            }
+        }
+        private void SetupCurrentMediaPositionData(TimeSpan currentPlaybackPosition)
+        {
+            var formattingPattern = @"hh\:mm\:ss";
+            if (CrossMediaManager.Current.Duration.Hours <= 0)
+                formattingPattern = @"mm\:ss";
+
+            var fullLengthString = CrossMediaManager.Current.Duration.ToString(formattingPattern);
+            PositionLabel.Text = $"{currentPlaybackPosition.ToString(formattingPattern)}/{fullLengthString}";
+
+            SongSlider.Value = currentPlaybackPosition.Ticks;
+        }
+        private void Current_MediaItemChanged(object sender, MediaItemEventArgs e)
+        {
+            SetupCurrentMediaDetails(e.MediaItem);
+        }
+        private void Current_PositionChanged(object sender, PositionChangedEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                SetupCurrentMediaPositionData(e.Position);
+            });
+        }
+
+        private void Current_OnStateChanged(object sender, StateChangedEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                SetupCurrentMediaPlayerState(e.State);
+            });
         }
         private async void ToBack(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
         }
-        public void Menu(object sender, EventArgs args)
-        {
-
-            menu_counter++;
-
-            if (menu_counter % 2 != 0)
-            {
-                BackgroundImageSource = "Resources/drawable/background.png";
-                songPic.Source = "Resources/drawable/examle.png";
-            }
-            else if (menu_counter % 2 == 0)
-            {
-                BackgroundImageSource = "Resources/drawable/background2.png";
-                songPic.Source = "Resources/drawable/examle2.png";
-            }
-        } 
     }
 }
