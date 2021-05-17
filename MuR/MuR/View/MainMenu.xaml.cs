28ddac6ba54cfb3e7924d674770738e1c2779003
Anton <Antonfirsov@list.ru>
2021-05-17 23:04:08 +0300

    Menu created (no test yet)

--- MuR/MuR/View/MainMenu.xaml.cs
+++ MuR/MuR/View/MainMenu.xaml.cs
@@ -1,16 +1,22 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Xamarin.Forms;
-//using Xamarin.Forms.Xaml;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MediaManager;
+using MuR.Model;
 
-//namespace MuR.View
-//{
-    //[XamlCompilation(XamlCompilationOptions.Compile)]
-    //public partial class MainMenu : MasterDetailPage
-    //{
-        
-    //}
-//}
+namespace MuR.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainMenu : Shell
+    {
+        public async void PlayerShellContentClicked(object sender, EventArgs args)
+        {
+            foreach (var item in await App.Database.SelectAllFromTable<MuR.Model.SQLiteObjects.Audio>())
+                CrossMediaManager.Current.Queue.Add(CrossFileManipulation.GetAudio(item.UriFile));
+        }
+    }
+}
